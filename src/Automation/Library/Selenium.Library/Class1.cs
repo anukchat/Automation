5fc31a1a1eb21130b64d3ae62867abd31b78c627
Anukool89 <chaturvedianukool@gmail.com>
2019-01-17 15:28:45 +0530

    clean up done

--- src/Automation/Library/Selenium.Library/Class1.cs
+++ src/Automation/Library/Selenium.Library/Class1.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Selenium.Library
+﻿namespace Selenium.Library
 {
     public class Class1
     {
