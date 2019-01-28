0d767eb1eaad9803274e21af360b1a754c3de6ef
Anukool89 <chaturvedianukool@gmail.com>
2019-01-28 17:14:31 +0530

    added some Selenium library, need more changes, not yet stablized

---
+++ src/Automation/Library/Selenium.Library/Extensions/DriverExtensions.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium.Library.Extensions
+{
+    public static class DriverExtensions
+    {
+        public static IWebElement FindElement(this ISearchContext context, By by, int timeOut = 20)
+        {
+            IWebElement element=null;
+            try
+            {
+                var wait = new DefaultWait<ISearchContext>(context);
+                wait.Timeout = TimeSpan.FromSeconds(timeOut);
+                return wait.Until(ctx =>
+                {
+                    element = ctx.FindElement(by);
+                    if (!element.Displayed)
+                        return null;
+                    return element;
+                });
+            }
+            
+            catch (NoSuchElementException ex)
+            {
+                Console.WriteLine("OOPs!! Unable to find the mentioned element!");
+            }
+            return element;
+        }
+
+    }
+}
--- src/Automation/Page Repository/Selenium.Page.Repository/Pages/SeleniumLogInPage.cs
+++ src/Automation/Page Repository/Selenium.Page.Repository/Pages/SeleniumLogInPage.cs
@@ -7,6 +7,15 @@ namespace Selenium.Page.Repository.Pages
 {
     public class SeleniumLogInPage : ILogIn
     {
+        #region Locators: ID
+        private readonly By logInUserNameID = By.Id("login_username");
+        private readonly By logInPasswordID = By.Id("login_password");
+        #endregion
+
+        #region Locators: XPath
+        private readonly By logInButtonXPath = By.XPath("//*[@id=\"login\"]/div[4]/input");
+        #endregion
+
         private IWebDriver _driver;
 
         public SeleniumLogInPage(SeleniumBase driver)
@@ -21,9 +30,9 @@ namespace Selenium.Page.Repository.Pages
 
         public void LogIn(string userName, string password)
         {
-            var usernameBox = _driver.FindElement(By.Id("login_username"));
-            var passwordBox = _driver.FindElement(By.Id("login_password"));
-            var logInButton = _driver.FindElement(By.XPath("//*[@id=\"login\"]/div[4]/input"));
+            var usernameBox = _driver.FindElement(logInUserNameID);
+            var passwordBox = _driver.FindElement(logInPasswordID);
+            var logInButton = _driver.FindElement(logInButtonXPath);
 
             usernameBox.SendKeys(userName);
             passwordBox.SendKeys(password);
